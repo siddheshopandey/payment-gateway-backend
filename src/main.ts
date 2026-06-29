f589ec55ee1b7e7adeaccc5d18cae399044c12cf
Siddhesh Pandey <siddhesh.o.pandey@gmail.com>
2026-06-29 22:32:02 +0530

    Allow Vercel frontend in CORS

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  });
+  origin: [
+    "http://localhost:5173",
+    "https://payment-gateway-frontend-murex.vercel.app",
+  ],
+  credentials: true,
+});
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
